d456dd43ef58a47ceae87f854f8bd997c82a7807
karthikha-shree <karthikha.2006@gmail.com>
2025-10-14 15:19:00 +0530

    dploy vite-congig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './',
+  base: '/react_portflio/', // 👈 IMPORTANT: must match your repo name exactly
   build: {
-    outDir: 'docs', // 👈 important
+    outDir: 'dist', // default output folder
   },
 })

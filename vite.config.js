2b951f25a0ed7c5e9cd4491104bab260769187d4
karthikha-shree <karthikha.2006@gmail.com>
2025-10-14 15:43:47 +0530

    check

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react_portflio/', // 👈 IMPORTANT: must match your repo name exactly
-  build: {
-    outDir: 'dist', // default output folder
-  },
-})
+  base: '/react_portflio/',
+});
